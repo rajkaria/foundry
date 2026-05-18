33444a648e377cf8b2ad92b76e41c6dca608e7b0
Raj <rajkaria98@gmail.com>
2026-05-18 12:12:00 +0530

    feat(0gkit-core): export public barrel

--- packages/0gkit-core/src/index.ts
+++ packages/0gkit-core/src/index.ts
@@ -1,1 +1,24 @@
-export const __0gkitCore = "scaffold";
+export {
+  ZeroGError,
+  ConfigError,
+  NetworkError,
+  ChainError,
+  AttestationError,
+  type ZeroGErrorCode,
+} from "./errors.js";
+export {
+  networks,
+  aristotle,
+  galileo,
+  local,
+  getNetwork,
+  type NetworkName,
+  type NetworkPreset,
+} from "./networks.js";
+export { type Receipt } from "./receipt.js";
+export {
+  createClient,
+  buildChain,
+  type CreateClientOptions,
+  type ZeroGClient,
+} from "./client.js";
